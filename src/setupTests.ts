ce11001a8d1268d2b296b6aa0aac8dc8228d8d52
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-07-14 15:04:01 -0300

    Configure redux-mock-store and jest-react-hooks-shallow

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,7 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
 import Enzyme from 'enzyme';
 import Adpater from 'enzyme-adapter-react-16';
 
+import enableHooks from 'jest-react-hooks-shallow';
+
 Enzyme.configure({ adapter: new Adpater() });
+
+enableHooks(jest);
